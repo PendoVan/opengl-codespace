711b9202b0b6b66bd105466d1ba8fa9bc33b9ce4
MasaruC <juanmasarucamposluque@gmail.com>
2026-04-12 00:50:05 +0000

    Test

--- Tareas/Tarea1.cpp
+++ Tareas/Tarea1.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
 
 void init(void)
 {
-    glClearColor(0.2,0.1,0.3,0.0);
+    glClearColor(0.0,0.0,0.0,0.0);
     glShadeModel(GL_FLAT);
 }
 
@@ -103,46 +103,21 @@ void drawAxes(GLfloat left, GLfloat right, GLfloat bottom, GLfloat top)
     glPopMatrix();
 }
 
-void drawGear(int teeth, GLfloat innerRadius, GLfloat outerRadius, GLfloat centerX, GLfloat centerY, GLfloat red, GLfloat green, GLfloat blue)
-{
-    const GLfloat step = M_PI / teeth;
-
-    glPushMatrix();
-    glColor3f(red, green, blue);
-    glEnable(GL_LINE_LOOP);
-    glLineStipple(1, 0x00FF);
-    glLineWidth(3.0f);
-    glBegin(GL_LINE_LOOP);
-
-    for (int t = 0; t < teeth; ++t)
-    {
-        GLfloat baseAngle = 2.0f * M_PI * t / teeth;
-
-        glVertex2f(centerX + outerRadius * cos(baseAngle), centerY + outerRadius * sin(baseAngle));
-        glVertex2f(centerX + outerRadius * cos(baseAngle + step), centerY + outerRadius * sin(baseAngle + step));
-        glVertex2f(centerX + innerRadius * cos(baseAngle + step), centerY + innerRadius * sin(baseAngle + step));
-        glVertex2f(centerX + innerRadius * cos(baseAngle + 2.0f * step), centerY + innerRadius * sin(baseAngle + 2.0f * step));
-    }
-
-    glEnd();
-    glDisable(GL_LINE_LOOP);
-    glPopMatrix();
-}
-
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    //drawAxes(ORTHO_LEFT, ORTHO_RIGHT, ORTHO_BOTTOM, ORTHO_TOP);
+    drawAxes(ORTHO_LEFT, ORTHO_RIGHT, ORTHO_BOTTOM, ORTHO_TOP);
+
+    const int n = 8; // puntos de la figura rellena
+    const int m = 10; // puntos de la figura punteada
+    const GLfloat radio = 5.0f;
 
     //drawShape(int points, GLfloat radius, GLfloat centerX, GLfloat centerY, DrawType mode, GLfloat red, GLfloat green, GLfloat blue)
     // DrawType: DRAW_FILL, DRAW_STROKE, DRAW_DASHED
-    drawShape(100, 4.0f, 0.0f, 0.0f, DRAW_DASHED, 0.0f, 0.7f, 0.2f);
-    drawShape(100, 7.0f, 0.0f, 0.0f, DRAW_DASHED, 1.0f, 0.6f, 0.0f);
+    drawShape(n, radio, 0.0f, 0.0f, DRAW_FILL, 0.0f, 0.7f, 0.2f);
+    drawShape(m, radio, 0.0f, 0.0f, DRAW_DASHED, 1.0f, 0.6f, 0.0f);
 
-    //-----------------------------------------------------------------
-    drawGear(12, 4.0f, 7.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
-    //-----------------------------------------------------------------
     glFlush();
 }
 
--- Tareas/Tarea2.cpp
+++ Tareas/Tarea2.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
 
 void init(void)
 {
-    glClearColor(0.2,0.1,0.3,0.0);
+    glClearColor(0.0,0.0,0.0,0.0);
     glShadeModel(GL_FLAT);
 }
 
@@ -103,37 +103,11 @@ void drawAxes(GLfloat left, GLfloat right, GLfloat bottom, GLfloat top)
     glPopMatrix();
 }
 
-void drawGear(int teeth, GLfloat innerRadius, GLfloat outerRadius, GLfloat centerX, GLfloat centerY, GLfloat red, GLfloat green, GLfloat blue)
-{
-    const GLfloat step = M_PI / teeth;
-
-    glPushMatrix();
-    glColor3f(red, green, blue);
-    glEnable(GL_LINE_LOOP);
-    glLineStipple(1, 0x00FF);
-    glLineWidth(3.0f);
-    glBegin(GL_LINE_LOOP);
-
-    for (int t = 0; t < teeth; ++t)
-    {
-        GLfloat baseAngle = 2.0f * M_PI * t / teeth;
-
-        glVertex2f(centerX + outerRadius * cos(baseAngle), centerY + outerRadius * sin(baseAngle));
-        glVertex2f(centerX + outerRadius * cos(baseAngle + step), centerY + outerRadius * sin(baseAngle + step));
-        glVertex2f(centerX + innerRadius * cos(baseAngle + step), centerY + innerRadius * sin(baseAngle + step));
-        glVertex2f(centerX + innerRadius * cos(baseAngle + 2.0f * step), centerY + innerRadius * sin(baseAngle + 2.0f * step));
-    }
-
-    glEnd();
-    glDisable(GL_LINE_LOOP);
-    glPopMatrix();
-}
-
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    //drawAxes(ORTHO_LEFT, ORTHO_RIGHT, ORTHO_BOTTOM, ORTHO_TOP);
+    drawAxes(ORTHO_LEFT, ORTHO_RIGHT, ORTHO_BOTTOM, ORTHO_TOP);
 
     //drawShape(int points, GLfloat radius, GLfloat centerX, GLfloat centerY, DrawType mode, GLfloat red, GLfloat green, GLfloat blue)
     // DrawType: DRAW_FILL, DRAW_STROKE, DRAW_DASHED
@@ -141,7 +115,26 @@ void display(void)
     drawShape(100, 7.0f, 0.0f, 0.0f, DRAW_DASHED, 1.0f, 0.6f, 0.0f);
 
     //-----------------------------------------------------------------
-    drawGear(12, 4.0f, 7.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+    glPushMatrix();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1, 0x00FF);
+    glLineWidth(3.0f);
+    glBegin(GL_LINE_LOOP);
+    int vertices = 16;
+    for (int i = 0; i < vertices; ++i)
+    {
+        GLfloat x, y;
+        GLfloat ang = 2.0f * M_PI * i / vertices;
+        GLfloat radius = (i % 2 == 0) ? 7.0f : 4.0f;
+        x = radius * cos(ang);
+        y = radius * sin(ang);
+        glVertex2f(x, y);
+    }
+
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+    glPopMatrix();
     //-----------------------------------------------------------------
     glFlush();
 }
